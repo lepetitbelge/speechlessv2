7ae9568e7975591814a53297ad24c3675936eab8
christophegillis <chris.tim.gillis@gmail.com>
2019-02-24 19:29:09 +0200

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_164621) do
+ActiveRecord::Schema.define(version: 2019_02_24_172254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contributions", force: :cascade do |t|
+    t.integer "start_position"
+    t.integer "end_position"
+    t.bigint "speech_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["speech_id"], name: "index_contributions_on_speech_id"
+    t.index ["user_id"], name: "index_contributions_on_user_id"
+  end
 
   create_table "speakers", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +33,20 @@ ActiveRecord::Schema.define(version: 2019_02_24_164621) do
     t.date "date_of_death"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "speeches", force: :cascade do |t|
+    t.string "title"
+    t.date "date"
+    t.string "country"
+    t.string "city"
+    t.string "category"
+    t.string "content"
+    t.time "duration"
+    t.bigint "speaker_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["speaker_id"], name: "index_speeches_on_speaker_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +62,7 @@ ActiveRecord::Schema.define(version: 2019_02_24_164621) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contributions", "speeches"
+  add_foreign_key "contributions", "users"
+  add_foreign_key "speeches", "speakers"
 end
