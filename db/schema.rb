08eda342d173a19bd0bc2a2fe8e0f5127e5ddf6e
christophegillis <chris.tim.gillis@gmail.com>
2019-02-24 19:36:48 +0200

    added content columns to Contribution and Comment models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_172254) do
+ActiveRecord::Schema.define(version: 2019_02_24_173458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "contribution_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "content"
+    t.index ["contribution_id"], name: "index_comments_on_contribution_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "contributions", force: :cascade do |t|
     t.integer "start_position"
@@ -22,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_02_24_172254) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["speech_id"], name: "index_contributions_on_speech_id"
     t.index ["user_id"], name: "index_contributions_on_user_id"
   end
@@ -62,6 +73,8 @@ ActiveRecord::Schema.define(version: 2019_02_24_172254) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "contributions"
+  add_foreign_key "comments", "users"
   add_foreign_key "contributions", "speeches"
   add_foreign_key "contributions", "users"
   add_foreign_key "speeches", "speakers"
