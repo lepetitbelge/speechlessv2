7624a09087595abec6ac8b8779da5288034c893d
christophegillis <chris.tim.gillis@gmail.com>
2019-02-24 18:39:29 +0200

    added speaker model

---
+++ db/migrate/20190224163828_create_speakers.rb
@@ -0,0 +1,12 @@
+class CreateSpeakers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :speakers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.date :date_of_death
+
+      t.timestamps
+    end
+  end
+end
