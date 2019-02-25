7113c5e1acfd1b3042b8607bfd3138f247c4b2ed
christophegillis <chris.tim.gillis@gmail.com>
2019-02-25 18:02:51 +0200

    added basic logic to update and destroy function

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,24 +1,41 @@
 class ContributionsController < ApplicationController
+  before_action
   def create
     # maybe need to make a speech here, can't test this fully until we have a speech view
     @contribution = Contribution.new(contribution_params)
+    @contribution.user = current_user
+    @contribution.speech = find_speech
     if @contribution.save
-      puts "We might do this with AJAX"
+      puts "Save went well, we might render with AJAX"
     else
-      puts "This went awfully wrong"
+      puts "Save went awfully wrong"
+    end
   end
 
   def update
-
+    @contribution.update(contribution_update_params)
+    if @contribution.save
+      puts "Update went well, we might render with AJAX"
+    else
+      puts "Update went awfully wrong"
+    end
   end
 
   def destroy
-
+    puts "redirect to same speech page" if @contribution.destroy
   end
 
   private
 
   def contribution_params
     params.require(:contribution).permit(:content, :start_position, :end_position)
+  end
+
+  def contribution_update_params
+    params.require(:contribution).permit(:content)
+  end
+
+  def find_speech
+    @speech = Speech.find(params[:speech_id])
   end
 end
