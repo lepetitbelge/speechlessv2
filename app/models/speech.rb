4a012141a5e2ae41c7f5ef983db2c7c65f80b683
lepetitbelge <44371329+lepetitbelge@users.noreply.github.com>
2019-02-25 19:15:04 +0200

    Update speech.rb

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -4,15 +4,10 @@ class Speech < ApplicationRecord
 
   has_many :contributions, dependent: :destroy
 
-  validates_presence_of :title, :date, :content, :category
-  validate :city_or_country
+  validates_presence_of :title, :date, :content, :category, :country
   validate :date_cannot_be_in_future
 
   private
-
-  def city_or_country
-    errors.add(:city, "or country must be present") unless city.present? || country.present?
-  end
 
   def date_cannot_be_in_future
     errors.add(:date, "can't be in the future") if Date.today < date
