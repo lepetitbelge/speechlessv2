2b95af55f0bee5d4acaaf820c654d3f187933224
arosty <arosty@gmx.net>
2019-02-25 11:22:20 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :speeches, only: %i[index show] do
+    resources :contributions, only: :create
+  end
+
+  resources :contributions, only: %i[update destroy] do
+    resources :votes, only: %i[upvote downvote]
+    resource :comments, only: :create
+  end
+
+  resources :comments, only: %i[update destroy] do
+    resources :votes, only: %i[upvote downvote]
+  end
+
+  resources :speakers, only: :show
+
+  resources :users, only: :show
+
+  get :profile, to: 'users#profile'
+
+  namespace :admin do
+    resources :speeches, only: %i[new create edit update destroy]
+    resources :speakers, only: %i[new create edit update destroy]
+  end
 end
