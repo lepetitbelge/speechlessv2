065127e3da2d8d195790b01f543490a6f99bfdec
christophegillis <chris.tim.gillis@gmail.com>
2019-02-25 14:09:05 +0200

    created initial validations

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,4 +4,7 @@ class Contribution < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :votes, as: :votable, dependent: :destroy
+
+  validates_presence_of :content, :start_position, :end_position
+  validates :content, length: { maximum: 300, too_long: "can't have more than %{count} characters" }
 end
