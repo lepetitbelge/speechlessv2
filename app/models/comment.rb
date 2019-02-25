7b9bdd5d51651bf5fff81d5c057d17257c8e0695
arosty <arosty@gmx.net>
2019-02-25 14:03:17 +0200

    added max 300 characters for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,5 @@ class Comment < ApplicationRecord
   has_many :votes, as: :votable, dependent: :destroy
 
   validates_presence_of :content, :contribution
+  validates :content, length: { maximum: 300, too_long: "can't have more than %{count} characters" }
 end
