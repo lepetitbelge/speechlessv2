b50a5cd1122fc2dcd8455a14e13ab278f559ceed
arosty <arosty@gmx.net>
2019-02-25 12:31:26 +0200

    done validations for votable and value

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
+
+  validates_presence_of :votable, :value
+  validates :value, inclusion: { in: -1..1, message: "must be -1, 0 or 1" }
 end
