e93c72b55c8c69e326a35e6a1a9cabc8cc984e67
christophegillis <chris.tim.gillis@gmail.com>
2019-02-25 12:36:45 +0200

    adapted validation with custom method

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,6 +1,9 @@
 class Speaker < ApplicationRecord
-  validate_presence_of :first_name
-  validates :last_name
-  validates :date_of_birth
-  validates :date_of_death
+  validate :first_or_last_name
+
+  private
+
+  def first_or_last_name
+    errors.add(:first_name, "or last name must be present") unless first_name.present? || last_name.present?
+  end
 end
