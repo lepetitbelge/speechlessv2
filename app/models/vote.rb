79ceb6b1095498b16bc7ce0978ae7d76dc72e1a7
arosty <arosty@gmx.net>
2019-02-25 17:02:30 +0200

    added uniqueness of user vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,5 @@ class Vote < ApplicationRecord
 
   validates_presence_of :votable, :value
   validates :value, inclusion: { in: [-1, 0, 1], message: "must be -1, 0 or 1" }
+  validates :user, uniqueness: { scope: %i[votable_id votable_type] }
 end
