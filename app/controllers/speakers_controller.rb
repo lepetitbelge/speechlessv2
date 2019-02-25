0dec43ec3ca02bc81b67c7755eff51db77f78c9c
arosty <arosty@gmx.net>
2019-02-25 16:02:00 +0200

    started with show action

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,11 +1,18 @@
 class SpeakersController < ApplicationController
+  before_action :find_speaker, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: :show
+
   def show
+    @speeches = Speech.where(speaker: self)
   end
 
   def new
+    @speaker = Speaker.new
   end
 
   def create
+    @speaker = Speaker.new(speaker_params)
+
   end
 
   def edit
@@ -15,5 +22,15 @@ class SpeakersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def find_speaker
+    @speaker = Speaker.find(params[:id])
+  end
+
+  def speaker_params
+    params.require(:speaker).permit(:first_name, :last_name, :date_of_birth, :date_of_death)
   end
 end
