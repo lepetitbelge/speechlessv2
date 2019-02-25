b3b3c675cfe6c7c75410a344b121f1c52f2b77ae
christophegillis <chris.tim.gillis@gmail.com>
2019-02-25 16:23:45 +0200

    updated date cannot be in future

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -6,10 +6,15 @@ class Speech < ApplicationRecord
 
   validates_presence_of :title, :date, :content, :category
   validate :city_or_country
+  validate :date_cannot_be_in_future
 
   private
 
   def city_or_country
     errors.add(:city, "or country must be present") unless city.present? || country.present?
+  end
+
+  def date_cannot_be_in_future
+    errors.add(:date, "can't be in the future") if Date.today < date
   end
 end
