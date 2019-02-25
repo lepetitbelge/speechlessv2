ac9f34571cc9fe04964e0e4ec21551a2ad6c6ee0
christophegillis <chris.tim.gillis@gmail.com>
2019-02-25 15:06:31 +0200

    added a validation for country or city

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -5,5 +5,11 @@ class Speech < ApplicationRecord
   has_many :contributions, dependent: :destroy
 
   validates_presence_of :title, :date, :content, :category
-  validate :country_or_county
+  validate :city_or_country
+
+  private
+
+  def city_or_country
+    errors.add(:city, "or country must be present") unless city.present? || country.present?
+  end
 end
