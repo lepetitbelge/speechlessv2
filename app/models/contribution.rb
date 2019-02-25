d269d71279573715329ed2214db461184f54b0a9
christophegillis <chris.tim.gillis@gmail.com>
2019-02-25 14:44:39 +0200

    added comments on including user in validates_presence_of

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -5,6 +5,7 @@ class Contribution < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, as: :votable, dependent: :destroy
 
-  validates_presence_of :content, :start_position, :end_position
-  validates :content, length: { maximum: 300, too_long: "can't have more than %{count} characters" }
+  # question: If we add :user to line 9. What happens when we delete a user (it has no dependent destroy)?
+  validates_presence_of :content, :start_position, :end_position, :speech
+  validates :content, length: { maximum: 1000, too_long: "can't have more than %{count} characters" }
 end
