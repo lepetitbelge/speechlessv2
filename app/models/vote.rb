0106aac4f7a14ee12c6e1e628c9749d73a3e6fbd
arosty <arosty@gmx.net>
2019-02-25 12:47:30 +0200

    array instead of range for inclusion

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
 
   validates_presence_of :votable, :value
-  validates :value, inclusion: { in: -1..1, message: "must be -1, 0 or 1" }
+  validates :value, inclusion: { in: [-1, 0, 1], message: "must be -1, 0 or 1" }
 end
