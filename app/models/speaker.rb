1a61013fae5e9cc5592c9752769d7baf99558b89
arosty <arosty@gmx.net>
2019-02-25 21:10:22 +0200

    add full name method to speaker to use it in front end

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -3,6 +3,16 @@ class Speaker < ApplicationRecord
 
   validate :first_or_last_name
 
+  def full_name
+    if self.first_name.nil?
+      "#{self.last_name}"
+    elsif self.last_name.nil?
+      "#{self.first_name}"
+    else
+      "#{self.first_name} #{self.last_name}"
+    end
+  end
+
   private
 
   def first_or_last_name
