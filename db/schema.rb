ae245fa1776a6e98f8358e927757fa3a6b1b43f1
david metta <davideliemetta@gmail.com>
2019-02-25 16:13:25 +0200

    removed category string from speeches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_140755) do
+ActiveRecord::Schema.define(version: 2019_02_25_141212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2019_02_25_140755) do
     t.date "date"
     t.string "country"
     t.string "city"
-    t.string "category"
     t.string "content"
     t.time "duration"
     t.bigint "speaker_id"
