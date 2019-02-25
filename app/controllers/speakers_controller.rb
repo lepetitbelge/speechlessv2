bc0ec24531e86ae7635b84fa2cc59cb3d80d741e
arosty <arosty@gmx.net>
2019-02-25 16:14:43 +0200

    added create update and destroy actions

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -3,7 +3,7 @@ class SpeakersController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def show
-    @speeches = Speech.where(speaker: self)
+    @speeches = Speech.where(speaker: @speaker)
   end
 
   def new
@@ -12,16 +12,36 @@ class SpeakersController < ApplicationController
 
   def create
     @speaker = Speaker.new(speaker_params)
-
+    if @speaker.save
+      flash[:notice] = "Speaker successfully created!"
+      redirect_to speaker_path(@speaker)
+    else
+      flash[:alert] = "Oops! Something went wrong. Speaker couldn't be created."
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @speaker.update(speaker_params)
+      flash[:notice] = "Speaker successfully updated!"
+      redirect_to speaker_path(@speaker)
+    else
+      flash[:alert] = "Oops! Something went wrong. Speaker couldn't be updated."
+      render :edit
+    end
   end
 
   def destroy
+    if @speaker.destroy
+      flash[:notice] = "Speech successfully deleted!"
+      redirect_to speeches_path
+    else
+      flash[:alert] = "Oops! Something went wrong. Speaker couldn't be deleted."
+      render :show
+    end
   end
 
   private
