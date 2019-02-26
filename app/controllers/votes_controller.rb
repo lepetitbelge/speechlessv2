2ba2610d74a19945422b8aea4c89869846dc4372
arosty <arosty@gmx.net>
2019-02-26 11:26:28 +0200

    updated and restructured functionality, redirect in upvote and downvote action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,44 +1,28 @@
 class VotesController < ApplicationController
   before_action :set_vote, only: :save_or_update_vote
-  after_action :save_vote, only: %i[upvote downvote]
   
   def upvote
-    new_value = 1
+    @new_value = 1
+    save_vote
+    redirect_to speech_path(@vote.votable.speech)
   end
 
   def downvote
-    new_value = -1
+    @new_value = -1
+    save_vote
+    redirect_to speech_path(@vote.votable.speech)
   end
 
   private
 
-  # def save_or_update_vote
-  #   @vote.id ? update_vote : save_vote
-  # end
-
   def save_vote
-    votable = params[:contribution_id].exist? ? Contribution.find(params[:contribution_id]) : Comment.find(params[:comment_id])
-    vote = Vote.where(user_id: current_user.id, votable_id: votable.id, votable_type: votable.class.to_s).first_or_initialize
-    vote.value += new_value
-    if vote.save
+    votable = params[:contribution_id] ? Contribution.find(params[:contribution_id]) : Comment.find(params[:comment_id])
+    @vote = Vote.where(user_id: current_user.id, votable_id: votable.id, votable_type: votable.class.to_s).first_or_initialize
+    @vote.value += @new_value
+    if @vote.save
       puts "Save went well, we might render with AJAX"
     else
-      puts "Save went awfully wrong"
+      puts "Save went wrong, maybe bacause the user alreade upvoted or downvoted and thus cannot upvote or downvote again (updating is possible though)"
     end
   end
-
-  # def update_vote
-  #   if @vote.update
-  #     "Update went well, we might render with AJAX"
-  #   else
-  #     "Update did not go awfully wrong, but value can't be greater than 1 or smaller than -1"
-  #   end
-  # end
-
-  # def set_vote
-  #   byebug
-  #   votable = params[:contribution_id].exist? ? Contribution.find(params[:contribution_id]) : Comment.find(params[:comment_id])
-  #   @vote = Vote.where(user_id: current_user.id, votable_id: votable.id, votable_type: votable.class.to_s).first_or_initialize
-  #   @vote.value += new_value
-  # end
 end
