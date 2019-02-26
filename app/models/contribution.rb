93e49d822b442f196d9720296a997e69d6d36423
arosty <arosty@gmx.net>
2019-02-26 14:04:04 +0200

    add validations for start and end position

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -8,4 +8,17 @@ class Contribution < ApplicationRecord
   # question: If we add :user to line 9. What happens when we delete a user (it has no dependent destroy)?
   validates_presence_of :content, :start_position, :end_position, :speech
   validates :content, length: { maximum: 1000, too_long: "can't have more than %{count} characters" }
+  validate :start_and_end_position
+
+  private
+
+  def start_and_end_position
+    if start_position && end_position
+      if (0..content.length).include? start_position && (0..content.length).include? end_position
+        errors.add(:start_position, "must be before end position") unless start_position < end_position
+      else
+        errors.add(:start_position, "and end position must be inside content")
+      end
+    end
+  end
 end
