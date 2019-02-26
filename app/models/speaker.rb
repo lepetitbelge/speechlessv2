23571193aa45d397568baa5850b9fee1c27206d9
david metta <davideliemetta@gmail.com>
2019-02-26 15:20:27 +0200

    added total votes, comments, and contributions to spkeaer model

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -13,6 +13,37 @@ class Speaker < ApplicationRecord
     end
   end
 
+  def speaker_total_contributions
+    speaker_contributions = 0
+    self.speeches.each do |speech|
+       speaker_contributions += speech.contributions.count
+    end
+    return speaker_contributions
+  end
+
+  def speaker_total_comments
+    speaker_comments = 0
+    self.speeches.each do |speech|
+      speech.contributions.each do |contribution|
+        speaker_comments += contribution.comments.count
+      end
+    end
+    return speaker_comments
+  end
+
+  def speaker_total_votes
+    speaker_total_votes = 0
+    self.speeches.each do |speech|
+      speech.contributions.each do |contribution|
+        speaker_total_votes += contribution.votes.count
+        contribution.comments.each do |comment|
+          speaker_total_votes += comment.votes.count
+        end
+      end
+    end
+    return speaker_total_votes
+  end
+
   private
 
   def first_or_last_name
