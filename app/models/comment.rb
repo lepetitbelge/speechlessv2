76f641c1bdfee10ca1fce0989cb003777f7b2454
arosty <arosty@gmx.net>
2019-02-26 11:48:51 +0200

    added the vote_sum method to comment controller as well

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,12 @@ class Comment < ApplicationRecord
 
   validates_presence_of :content, :contribution
   validates :content, length: { maximum: 300, too_long: "can't have more than %{count} characters" }
+
+  def vote_sum
+    value_sum = 0
+    Vote.where(votable_type: "Comment", votable_id: self.id).each do |vote|
+      value_sum += vote.value
+    end
+    return value_sum
+  end
 end
