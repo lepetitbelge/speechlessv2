b76fd75d3cbdd1afbde0ec680619245e15827ffc
arosty <arosty@gmx.net>
2019-02-26 20:45:07 +0200

    added level caluculation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates :username, presence: true
 
   def total_stats
-    stats = {contributions: 0, comments: 0, comments_votes: 0, contributions_votes: 0}
+    stats = {contributions: 0, comments: 0, contributions_votes: 0, comments_votes: 0}
     stats[:contributions] = self.contributions.count
     self.contributions.each do |contribution|
       stats[:contributions_votes] += contribution.vote_sum
@@ -25,5 +25,17 @@ class User < ApplicationRecord
   
   def total_votes
     self.total_stats[:comments_votes] + self.total_stats[:contributions_votes]
+  end
+
+  def level
+    byebug
+    current_stats = self.total_stats
+    case (5 * current_stats[:contributions] + 2 * current_stats[:comments] + current_stats[:contribution_votes] + current_stats[:comments_votes])
+    when 0..24 then return 1
+    when 25..150 then return 2
+    when 150..500 then return 3
+    when 500..1000 then return 4
+    else return 5
+    end
   end
 end
