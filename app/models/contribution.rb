8676bd986024abed483b4b33767eac890674041b
arosty <arosty@gmx.net>
2019-02-26 11:48:04 +0200

    added vote_sum method to contribution controller

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -8,4 +8,12 @@ class Contribution < ApplicationRecord
   # question: If we add :user to line 9. What happens when we delete a user (it has no dependent destroy)?
   validates_presence_of :content, :start_position, :end_position, :speech
   validates :content, length: { maximum: 1000, too_long: "can't have more than %{count} characters" }
+
+  def vote_sum
+    value_sum = 0
+    Vote.where(votable_type: "Contribution", votable_id: self.id).each do |vote|
+      value_sum += vote.value
+    end
+    return value_sum
+  end
 end
