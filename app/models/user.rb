cfe716a065915c1cccaf6bd3b4fba138819d9d77
arosty <arosty@gmx.net>
2019-02-26 20:51:52 +0200

    removed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   def level
     byebug
     current_stats = self.total_stats
-    case (5 * current_stats[:contributions] + 2 * current_stats[:comments] + current_stats[:contribution_votes] + current_stats[:comments_votes])
+    case (5 * current_stats[:contributions] + 2 * current_stats[:comments] + current_stats[:contributions_votes] + current_stats[:comments_votes])
     when 0..24 then return 1
     when 25..150 then return 2
     when 150..500 then return 3
