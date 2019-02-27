54900dd7c1d0d4b7d81e8a2094df3b7c825c18a2
christophegillis <chris.tim.gillis@gmail.com>
2019-02-27 17:49:49 +0200

    updaterd schema

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2019_02_27_120846) do
     t.index ["user_id"], name: "index_contributions_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "speakers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
