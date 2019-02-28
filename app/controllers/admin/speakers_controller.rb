430a2d68db12addbb26c76be65db09321a7b6a8d
arosty <arosty@gmx.net>
2019-02-28 11:30:31 +0200

    updated admin speeches view

---
+++ app/controllers/admin/speakers_controller.rb
@@ -0,0 +1,23 @@
+class Admin::SpeakersController < ApplicationController
+  def index
+    @speakers = Speaker.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
