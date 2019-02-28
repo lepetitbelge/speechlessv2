d33f831682d9e89578673f89e9c00065b8a49d55
arosty <arosty@gmx.net>
2019-02-28 10:02:01 +0200

    added photo field to speaker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_144119) do
+ActiveRecord::Schema.define(version: 2019_02_28_080103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_144119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "bio"
+    t.string "photo"
   end
 
   create_table "speeches", force: :cascade do |t|
