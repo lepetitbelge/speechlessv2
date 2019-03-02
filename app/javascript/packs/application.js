2936f7b3d4d22bb44ed8007d774bda12feec7337
david metta <davideliemetta@gmail.com>
2019-03-02 19:17:48 +0200

    contributions now hide/show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,10 @@
 import "bootstrap";
 import { hideComments } from './thread.js';
 import { showComments } from './thread.js';
+import { showContributions } from './thread.js';
+import { hideContributions } from './thread.js';
 
 hideComments();
 showComments();
+hideContributions();
+showContributions();
--- app/javascript/packs/thread.js
+++ app/javascript/packs/thread.js
@@ -1,6 +1,10 @@
-const comments = document.querySelector('.hidediv');
+const comments = document.querySelector('.hideDivComments');
+const contributions = document.querySelector('.hideDivContributions');
 const btnShow = document.querySelector('.btn-show');
 const btnHide = document.querySelector('.btn-hide');
+const btnShowC = document.querySelector('.btn-show-c');
+const btnHideC = document.querySelector('.btn-hide-c');
+
 
 
 const showComments = () => {
@@ -15,5 +19,21 @@ const hideComments = () => {
   });
 };
 
+const hideContributions = () => {
+  btnHideC.addEventListener('click', () => {
+    contributions.style.display = 'none';
+    comments.style.display = 'none';
+  });
+};
+
+const showContributions = () => {
+  btnShowC.addEventListener('click', () => {
+    contributions.style.display = 'block';
+    comments.style.display = 'block';
+  });
+};
+
 export { showComments };
 export { hideComments };
+export { showContributions };
+export { hideContributions };
