e976c82cf6f6618e0cbc3cf4ae6dce5b4a979688
christophegillis <chris.tim.gillis@gmail.com>
2019-03-03 18:50:15 +0200

    migrated extension trgm

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_151130) do
+ActiveRecord::Schema.define(version: 2019_03_03_163400) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
