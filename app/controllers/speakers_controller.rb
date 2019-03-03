2c3744ac4d8ca069cabb07c4f68072947518d223
arosty <arosty@gmx.net>
2019-03-03 17:09:47 +0200

    renewde banner, with stats in the bottom, cards under it in order

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -6,11 +6,20 @@ class SpeakersController < ApplicationController
 
   def show
     @speaker = Speaker.find(params[:id])
-    @speeches = Speech.where(speaker: @speaker)
+    speech_suggestions
     get_info_summary
   end
 
   private
+
+  def speech_suggestions
+    speeches = Speech.where(speaker: @speaker)
+    # TODO: ake suggestions (order, latest, all time)
+    # For now:
+    @all_time_speeches = speeches
+    @latest_speeches = speeches
+    # CHANGE LATER
+  end
 
   def get_info_summary
     url_base = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
