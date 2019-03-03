2f0490c9f2d7d59a0c54b1cbbdc7f7d30930fe62
arosty <arosty@gmx.net>
2019-03-03 16:15:05 +0200

    made banner for speaker page

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -15,10 +15,11 @@ class SpeakersController < ApplicationController
   def get_info_summary
     url_base = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
     @info_summary = JSON.parse(open(url_base + @speaker.full_name).read)['query']['pages'].values.first['extract']
-    3.times do
-      @info_summary.gsub!(/\s\([^\(\)]*\)/,'')
+    if @info_summary
+      @info_summary = @info_summary.match(/^[^\n]*/).to_s
+      3.times do
+        @info_summary.gsub!(/\s\([^\(\)]*\)/,'')
+      end
     end
-    @info_summary = "<p>#{@info_summary.gsub(/\n/,'</p><p>')}</p>"
-    byebug
   end
 end
