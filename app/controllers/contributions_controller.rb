1fd67396b5ab019c43e234182a4800540d8b4e11
arosty <arosty@gmx.net>
2019-03-04 12:18:25 +0200

    added selection_uui to contributions controller

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -16,7 +16,7 @@ class ContributionsController < ApplicationController
   end
 
   def update
-    if @contribution.update(contribution_params)
+    if @contribution.update(contribution_update_params)
       puts "Update went well, we might render with AJAX"
     else
       puts "Update went awfully wrong"
@@ -34,6 +34,10 @@ class ContributionsController < ApplicationController
   private
 
   def contribution_params
+    params.require(:contribution).permit(:content, :selection_uui)
+  end
+
+  def contribution_update_params
     params.require(:contribution).permit(:content)
   end
 
