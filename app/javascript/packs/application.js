3600874082a968eb8d642478b0a49f872d10a595
arosty <arosty@gmx.net>
2019-03-04 18:19:20 +0200

    deleted vote js files in app/js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,6 @@ import "bootstrap";
 import { initThreads } from '../components/thread';
 initThreads();
 
-import { initVotes } from '../components/votes';
-initVotes();
-
 import { initHighlighter } from '../components/highlight';
 initHighlighter();
 
