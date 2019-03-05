4a775f20b4068450f97611451a40db3c7f4bffd4
arosty <arosty@gmx.net>
2019-03-05 13:11:02 +0200

    deleted comments

--- app/javascript/components/show_current_contributions.js
+++ app/javascript/components/show_current_contributions.js
@@ -1,23 +1,3 @@
-// // When the user scrolls the page, execute myFunction
-
-
-// // Get the navbar
-// var navbar = document.querySelector(".navbar-lewagon");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function navbarSticky() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-// export { navbarSticky };
-
 const speechText = document.querySelector(".speech-text");
 
 const sticky = speechText.offsetTop;
