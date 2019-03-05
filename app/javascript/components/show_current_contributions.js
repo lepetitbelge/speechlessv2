e5d526b2211942ae93a85fdbfe834a346de59a8a
arosty <arosty@gmx.net>
2019-03-05 16:29:00 +0200

    first complete drafts, not working yet

--- app/javascript/components/show_current_contributions.js
+++ app/javascript/components/show_current_contributions.js
@@ -11,44 +11,29 @@ const thread = document.querySelector(".thread");
 //   });
 // };
 
-const makeAllInvisible = () => {
-  document.querySelectorAll(".comments").forEach(contributionThread => {
-    contributionThread.classList.remove('is-visible');
-  });
-};
-const checkIfSpanInViewpoint = (span) => {
+const makeCurrentsVisible = (span) => {
+  const identity = span.getAttribute('data-uui');
+  const contribution = document.getElementById(`uui-${identity}`).previousSibling;
   const location = span.getClientRects();
   const bottomIndex = location.length - 1;
   if (location[bottomIndex].bottom >= 0 && location[0].top <= window.innerHeight) {
-    // inside window
-    
+    contribution.classList.add('is-visible');
   } else {
-    console.log('nicht drin');
+    contribution.classList.remove('is-visible');
   }
 };
 
-const testSpan = document.querySelector(`[data-uui="a1c96h3uj2fq10r5bzy1kk"]`);
-// but the market is ...
+// const testSpan = document.querySelector(`[data-uui="a1c96h3uj2fq10r5bzy1kk"]`);
+// // but the market is ...
 
-console.log(window.scrollTop);
+// console.log(window.scrollTop);
 
 const updateCurrentContributions = () => {
   if (thread) {
     window.addEventListener('scroll', () => {
-      makeAllInvisible();
-      // makeCurrentsVisible();
-      console.log(window.innerHeight);
-      const location = testSpan.getClientRects();
-      console.log(location)
-      const bottomIndex = location.length - 1;
-      console.log(location[bottomIndex].bottom)
-      console.log(location[0].top)
-      if (location[bottomIndex].bottom >= 0 && location[0].top <= window.innerHeight) {
-        console.log('drin');
-      } else {
-        console.log('nicht drin');
-      }
-
+      document.querySelectorAll("span").forEach(span => {
+        makeCurrentsVisible(span);
+      });
       // console.log(testSpan.getClientRects()[2].bottom >= 0);
       if (window.scrollY >= 308) {
         thread.classList.add('thread-sticky');
