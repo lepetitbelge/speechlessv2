57e825030b372342f0ef1c309de9c14501ca41d1
christophegillis <chris.tim.gillis@gmail.com>
2019-03-05 19:41:15 +0200

    first attempt to do transition from video to wordcloud

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,6 @@ connectHighlightContribution();
 
 import { updateCurrentContributions } from '../components/show_current_contributions';
 updateCurrentContributions();
+
+import { transitionToWordcloud } from '../components/home_page_banner';
+transitionToWordcloud();
