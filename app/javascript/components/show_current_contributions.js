83e40e2d9956d3cd3ba0ed1748ccfa7df93eab30
arosty <arosty@gmx.net>
2019-03-05 17:17:30 +0200

    debugged and working

--- app/javascript/components/show_current_contributions.js
+++ app/javascript/components/show_current_contributions.js
@@ -1,32 +1,19 @@
-const speechText = document.querySelector(".speech-text");
-
-const sticky = speechText.offsetTop;
-
 const thread = document.querySelector(".thread");
-
-// const makeCurrentsVisible = () => {
-//   console.log(window.querySelectorAll(".comments"))
-//   window.querySelectorAll(".comments").forEach(contributionThread => {
-//     contributionThread.classList.add('is-visible');
-//   });
-// };
 
 const makeCurrentsVisible = (span) => {
   const identity = span.getAttribute('data-uui');
-  const contribution = document.getElementById(`uui-${identity}`).previousSibling;
-  const location = span.getClientRects();
-  const bottomIndex = location.length - 1;
-  if (location[bottomIndex].bottom >= 0 && location[0].top <= window.innerHeight) {
-    contribution.classList.add('is-visible');
-  } else {
-    contribution.classList.remove('is-visible');
+  let contribution = document.getElementById(`uui-${identity}`);
+  if (contribution) {
+    contribution = contribution.parentNode;
+    const location = span.getClientRects();
+    const bottomIndex = location.length - 1;
+    if (location[bottomIndex].bottom >= 0 && location[0].top <= window.innerHeight) {
+      contribution.classList.remove('invisible');
+    } else {
+      contribution.classList.add('invisible');
+    }
   }
-};
-
-// const testSpan = document.querySelector(`[data-uui="a1c96h3uj2fq10r5bzy1kk"]`);
-// // but the market is ...
-
-// console.log(window.scrollTop);
+}
 
 const updateCurrentContributions = () => {
   if (thread) {
@@ -34,7 +21,6 @@ const updateCurrentContributions = () => {
       document.querySelectorAll("span").forEach(span => {
         makeCurrentsVisible(span);
       });
-      // console.log(testSpan.getClientRects()[2].bottom >= 0);
       if (window.scrollY >= 308) {
         thread.classList.add('thread-sticky');
       } else {
