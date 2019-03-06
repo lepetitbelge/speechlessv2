05ad0a339e56d6b98f693d7d1115ab1a90a78f85
david metta <davideliemetta@gmail.com>
2019-03-06 12:37:37 +0200

    added photo url to user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :photo_url])
     # devise_parameter_sanitizer.permit(:account_update, keys: [:addsomething])
   end
 
